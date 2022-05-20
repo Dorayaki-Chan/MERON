26a4a402e37263e1c18e00066a5870b8855c8328
Dorayaki-Chan <zero.kotam@gmail.com>
2022-05-20 21:52:47 +0900

    htmlページ追加

--- src/MERON/blog/urls.py
+++ src/MERON/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world, name='helloWorld'),
+    path('test', views.hello_world, name='helloWorld'),
+    path('',views.index,name='index'),
+    path('result',views.result,name='result'),
 ]
--- src/MERON/blog/views.py
+++ src/MERON/blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 def hello_world(request):
     return render(request, 'blog/helloWorld.html', {})
+
+def index(request):
+    return render(request,'blog/index.html',{})
+
+def result(request):
+    strT = request.POST["dish"]
+    return render(request,'blog/result.html',{"result":strT})
