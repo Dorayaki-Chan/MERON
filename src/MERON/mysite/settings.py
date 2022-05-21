b3f421b4bb0929e991372e726b856142dbfd3895
Dorayaki-Chan <zero.kotam@gmail.com>
2022-05-22 02:29:29 +0900

    解決した説

--- src/MERON/mysite/settings.py
+++ src/MERON/mysite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.SimpleMiddleware', 
+    # 'middleware.SimpleMiddleware', 
 ]
 
 ROOT_URLCONF = 'mysite.urls'
