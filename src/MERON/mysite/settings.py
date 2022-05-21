ca62b7c2bb7b2b712856ea0732abb4726c0b6212
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-22 01:38:56 +0900

    setting

--- src/MERON/mysite/settings.py
+++ src/MERON/mysite/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.SimpleMiddleware', 
 ]
 
 ROOT_URLCONF = 'mysite.urls'
