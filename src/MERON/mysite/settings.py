fe6e258edad1ab638481aeeddf457492c1763e21
sho0318 <70184797+sho0318@users.noreply.github.com>
2022-05-22 01:14:01 +0900

    template 一区切り

--- src/MERON/mysite/settings.py
+++ src/MERON/mysite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 # pythonanywhere用
-## ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+# ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
