2e7bd94f23c40e18fef998abf4a91e95de18c15e
Dorayaki-Chan <zero.kotam@gmail.com>
2022-05-22 13:29:30 +0900

    例外処理変更

--- src/MERON/blog/views.py
+++ src/MERON/blog/views.py
@@ -18,24 +18,25 @@ def index(request):
 def result(request):
     try:
         img = request.FILES["dish_pic"]
-        # URL用
-        # img = io.BytesIO(requests.get('https://cdn.discordapp.com/attachments/872060747079897088/977489461346701332/test.jpeg').content)
-        dish = WhatDishName(img)
-        print("\n\n\n"+"AI使ったぞ"+dish.dish_name+"\n\n\n\n")
-        dishClass = DishMaked(dish.dish_name)
-        Kcal, Protein, Lipids, Carbohydrate = eiyouSum(dishClass)
-
-        return render(request,'blog/index.html',{"result":dish.dish_name, "dd_name":dish.dish_name, "zairyos":dishClass.syokuzais, "flag":1, "eiyouso":{'Kcal':Kcal, 'Protein':Protein, 'Lipids':Lipids, 'Carbohydrate':Carbohydrate}})
     except:
         try:
             dish_name = request.POST["input_dish_name"]
-            dishClass = DishMaked(dish_name)
-            print("\n\n\n"+"入力値観たぞ"+dish_name+"\n\n\n\n")
-            Kcal, Protein, Lipids, Carbohydrate = eiyouSum(dishClass)
-            return render(request,'blog/index.html',{"result":dish_name, "dd_name":dish_name, "zairyos":dishClass.syokuzais, "flag":1, "eiyouso":{'Kcal':Kcal, 'Protein':Protein, 'Lipids':Lipids, 'Carbohydrate':Carbohydrate}})
-
         except:
             return render(request,'blog/index.html',{"flag":0})
+
+        dishClass = DishMaked(dish_name)
+        print("\n\n\n"+"入力値観たぞ"+dish_name+"\n\n\n\n")
+        Kcal, Protein, Lipids, Carbohydrate = eiyouSum(dishClass)
+        return render(request,'blog/index.html',{"result":dish_name, "dd_name":dish_name, "zairyos":dishClass.syokuzais, "flag":1, "eiyouso":{'Kcal':Kcal, 'Protein':Protein, 'Lipids':Lipids, 'Carbohydrate':Carbohydrate}})
+    
+    # URL用
+    # img = io.BytesIO(requests.get('https://cdn.discordapp.com/attachments/872060747079897088/977489461346701332/test.jpeg').content)
+    dish = WhatDishName(img)
+    print("\n\n\n"+"AI使ったぞ"+dish.dish_name+"\n\n\n\n")
+    dishClass = DishMaked(dish.dish_name)
+    Kcal, Protein, Lipids, Carbohydrate = eiyouSum(dishClass)
+
+    return render(request,'blog/index.html',{"result":dish.dish_name, "dd_name":dish.dish_name, "zairyos":dishClass.syokuzais, "flag":1, "eiyouso":{'Kcal':Kcal, 'Protein':Protein, 'Lipids':Lipids, 'Carbohydrate':Carbohydrate}})
 
 def android(request):
     img = request.FILES['upload_file1']["name"]
